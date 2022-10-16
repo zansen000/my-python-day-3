c1fdd7f7dae16d9870bba9d755b0cb0ab8f8b851
zansen000 <95951004+zansen000@users.noreply.github.com>
2022-10-16 13:58:17 +0700

    Create python day 3.py

---
+++ python day 3.py
@@ -0,0 +1,27 @@
+#this is formatted strings
+
+
+#first = "john"
+#last = "smith"
+#message = first + '[' + last + '] is a coder '
+#msg = f'{first} [{last}] is a coder'
+#print (msg)
+
+#len strings
+
+#name = input("what is your name?")
+#print("so your length name is", len(name))
+
+#fomatted strings case
+
+#name = "python for beginners"
+#print(name.upper())
+#print(name.lower())
+#print(name.find('y'))
+#print(name.replace('beginners', 'absolute beginners'))
+#print(name)
+
+#in strings
+
+#name = "python was been code by zansen"
+#print('zansen' in name)
